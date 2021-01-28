7da4a53b5991357f09eb78ae2da4282985e52df3
antivitamila <tamila.gov@outlook.com>
2021-01-28 11:21:49 +0100

    Uebungen1

---
+++ Uebungen/uebung1-4/calc.php
@@ -0,0 +1,16 @@
+<?php
+
+$x = 10;
+$y= 10;
+
+$x = intval($x);
+$y = intval($y);
+
+$addieren = $x + $y;
+$subtrahieren = $x - $y;
+$multiplizieren = $x * $y;
+$dividieren = $x / $y;
+
+echo "Ergebnis: $addieren, $subtrahieren, $multiplizieren, $dividieren" ;
+
+?>
